fb9b3063709dcd0cbba305a36c40df93cb57f317
DemissenovDanial <123939899+DemissenovDanial@users.noreply.github.com>
2024-12-01 20:57:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 class Admins(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def set_password(self, password):
         self.password = generate_password_hash(password)
